b507d72f0ad22e85e81efb4fb28898c9c24b034c
andrew cooke <anderw@acooke.org>
2012-08-19 14:50:35 -0400

    dead

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Stalled waiting on
+http://stackoverflow.com/questions/11777914/using-python-and-libgpod-gtkpod-to-load-music-to-an-ipod-from-linux
+
+Switched to RockBox.  Next project UKYFF will combine this and earlier work on playlist generation (UYKFE).
+
--- src/ypod/gpod/_test/basics.py
+++ src/ypod/gpod/_test/basics.py
@@ -1,15 +1,19 @@
 
 from unittest import TestCase
 
-from gpod.gpod import itdb_device_set_sysinfo
+from gpod.gpod import itdb_device_set_sysinfo, ITDB_IPOD_GENERATION_CLASSIC_3
 from gpod.ipod import Database
 
 
 class BasicTest(TestCase):
 
     def test_load(self):
-        db = Database('/media/andrew@acooke.org ipod')
-        itdb_device_set_sysinfo(db._itdb.device, "ModelNumStr", "xB150")
+        db = Database('/media/andrew@acooke.org')
+#        db = Database('/mnt/usb')
+        itdb_device_set_sysinfo(db._itdb.device, "ModelNumStr", "C297")
+#        itdb_device_set_sysinfo(db._itdb.device, "ModelNumStr", "xB150")
+#        itdb_device_set_sysinfo(db._itdb.device, "ModelNumStr", ITDB_IPOD_GENERATION_CLASSIC_3)
+#        db.import_file("/home/andrew/project/ypod/music/ZZ Top/Tres Hombres/ZZ Top - 01 - Waitin' for the Bus.mp3")
         db.import_file('/home/andrew/project/ypod/music/Various/Underground Communication/Bassnectar - 07 - Kick It Complex (Bassnectar remix).mp3')
         db.copy_delayed_files()
         db.close()
